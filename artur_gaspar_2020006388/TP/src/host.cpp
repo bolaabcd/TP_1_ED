f50b567513ed02fe1e00c7308ebeb7007b186943
Artur <arthur.gaspar2011@gmail.com>
2021-11-26 10:17:04 -0300

    Implementadas todos métodos públicos menos add_url().

--- artur_gaspar_2020006388/TP/src/host.cpp
+++ artur_gaspar_2020006388/TP/src/host.cpp
@@ -20,20 +20,35 @@ Host::Host(std::string url)
 
 std::string Host::base_string()
 {
+    return this->host_string;
 }
 
 void Host::add_url(URL url)
 {
 }
+
 URL_Node *Host::get_first_url()
 {
+    return this->no_frontal;
 }
+
 void Host::remove_first_url()
 {
+    URL_Node *primeiro = this->no_frontal;
+    this->no_frontal = this->no_frontal->proximo;
+    delete primeiro;
+    this->tamanho--;
 }
+
 bool Host::vazio()
 {
+    return this->tamanho == 0;
 }
+
 void Host::limpar()
 {
+    while (!this->vazio())
+    {
+        this->remove_first_url();
+    }
 }
