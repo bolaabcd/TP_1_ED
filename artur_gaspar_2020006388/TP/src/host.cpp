854b5b573d207989626805fd5f1dae4fbe147801
Artur <arthur.gaspar2011@gmail.com>
2021-11-26 10:13:20 -0300

    Adicionado esqueleto dos métodos de Host.

--- artur_gaspar_2020006388/TP/src/host.cpp
+++ artur_gaspar_2020006388/TP/src/host.cpp
@@ -6,13 +6,34 @@
 
 #include "host.h"
 
-URL_Node::URL_Node(URL url):url(url){
-    this->proximo=nullptr;
+URL_Node::URL_Node(URL url) : url(url)
+{
+    this->proximo = nullptr;
 }
 
-Host::Host(std::string url){
+Host::Host(std::string url)
+{
     this->host_string = get_host_string(url);
     this->no_frontal = nullptr;
     this->tamanho = 0;
 }
 
+std::string Host::base_string()
+{
+}
+
+void Host::add_url(URL url)
+{
+}
+URL_Node *Host::get_first_url()
+{
+}
+void Host::remove_first_url()
+{
+}
+bool Host::vazio()
+{
+}
+void Host::limpar()
+{
+}
