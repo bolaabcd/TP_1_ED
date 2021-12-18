f8c4b7b7ccbb107108169ff41f8cd6556ceb56be
Artur <arthur.gaspar2011@gmail.com>
2021-12-18 17:37:50 -0300

    Adicionada opção de registrar memória.

--- artur_gaspar_2020006388/TP/src/escalonar.cpp
+++ artur_gaspar_2020006388/TP/src/escalonar.cpp
@@ -6,9 +6,53 @@
 
 #include <iostream>
 #include <fstream>
+#include <getopt.h>
 #include "msgassert.h"
+#include "memlog.h"
 #include "escalonador.h"
 #include "comando.h"
+
+// variavel globais para opcoes
+int regmem;
+
+void parse_args(int argc, char **argv)
+// Descricao: le as opcoes da linha de comando e inicializa variaveis.
+// Entrada: argc e argv.
+// Saida: regmem.
+{
+  // tratando caso de nao ter argumentos
+  if (argc == 1)
+  {
+    uso();
+    exit(1);
+  }
+
+  // variavel externa do getopt
+  extern char *optarg;
+
+  // variavel auxiliar
+  int c;
+
+  // inicializacao variaveis globais para opcoes
+  regmem = 0;
+  bool p = false;
+
+  // getopt - letra indica a opcao, : junto a letra indica parametro
+  while ((c = getopt(argc, argv, "l")) != EOF)
+  {
+    switch (c)
+    {
+    case 'l':
+      regmem = 1;
+      break;
+    case 'h':
+    default:
+      uso();
+      exit(1);
+    }
+  }
+}
+
 
 void uso()
 // Descricao: imprime as opcoes de uso
@@ -17,6 +61,7 @@ void uso()
 {
     std::cerr << "escalonador" << std::endl;
     std::cerr << "\t<arq>\t\t(nome do arquivo de entrada, argumento obrigatorio)" << std::endl;
+    std::cerr << "\t-l \t\t(registrar padrao de acesso e localidade)" << std::endl;
 }
 
 std::string get_nome_saida(std::string nome_entrada)
@@ -29,14 +74,22 @@ std::string get_nome_saida(std::string nome_entrada)
     return nome_entrada.substr(0, pos_ext) + "-out" + nome_entrada.substr(pos_ext);
 }
 
-int main(int argc, char **argv)
+std::string get_nome_registro(std::string nome_entrada)
+// Descricao: gera o nome do arquivo de registro.
+// Entrada: nome do arquivo de entrada.
+// Saida: nome do arquivo de registro.
 {
-    if (argc != 2)
-    {
-        uso();
-        exit(1);
-    }
+    size_t pos_ext = nome_entrada.find_last_of('.');
+    erroAssert(pos_ext != size_t(-1), "Nome de entrada invalido por nao possuir extensao.");
+    return nome_entrada.substr(0, pos_ext) + "-log" + nome_entrada.substr(pos_ext);
 
+}
+
+int main(int argc, char **argv)
+// Descricao: método principal do programa escalonar.
+// Entrada: argc e argv.
+// Saida: no arquivo especificado esqueve o resultado das operações especificadas.
+{
     // Inicializando arquivos e objetos principais.
     std::string nome_entrada(argv[1]);
     std::ifstream arq_entrada;
@@ -47,6 +100,12 @@ int main(int argc, char **argv)
 
     Escalonador escal(nome_saida);
     Comando com;
+    
+    // iniciar registro de acesso
+    iniciaMemLog(get_nome_registro(nome_entrada),regmem);
+
+    // ativar registro de acesso
+    ativaMemLog();
 
     // Executando comandos
     while (arq_entrada >> com)
@@ -59,5 +118,6 @@ int main(int argc, char **argv)
     escal.destruir();
     arq_entrada.close();
 
-    return 0;
+    // conclui registro de acesso
+    return finalizaMemLog();
 }
