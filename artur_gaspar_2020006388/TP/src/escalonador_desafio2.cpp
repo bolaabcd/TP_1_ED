be950dc5873884b86f5f5416011c1408152c3e93
Artur <arthur.gaspar2011@gmail.com>
2021-12-20 14:34:29 -0300

    Implementado desafio 2.

---
+++ artur_gaspar_2020006388/TP/src/escalonador_desafio2.cpp
@@ -0,0 +1,214 @@
+//---------------------------------------------------------------------
+// Arquivo      : escalona.cpp
+// Conteudo     : implementacao do tipo Escalona
+// Autor        : Artur Gaspar da Silva (artur.gaspar@dcc.ufmg.br)
+//---------------------------------------------------------------------
+
+#include "escalonador.h"
+#include "msgassert.h"
+#include "host.h"
+#include "url.h"
+
+Escalonador::Escalonador(std::string nome_saida)
+// Descricao: constrói o tipo escalonador a partir do arquivo de saída, que é aberto aqui.
+// Entrada: caminho do arquivo de saída.
+// Saida: objeto Escalonador instanciado.
+{
+    this->arq_saida.open(nome_saida, std::ofstream::out);
+
+    erroAssert(arq_saida.good(), "Nao foi possivel abrir o arquivo de saida!");
+}
+
+void Escalonador::executar_comando(Comando &com)
+// Descricao: executa um comando.
+// Entrada: objeto do tipo Comando representando o comando a ser executado.
+// Saida: depende do comando, alguns imprimem algo no arquivo de saída, outros só deletam
+// URLs, por exemplo.
+{
+    switch (com.get_id())
+    {
+    case 0: // add_urls
+        while (com.more_urls())
+        {
+            try
+            {
+                this->add_url(com.get_url());
+            }
+            catch (const std::invalid_argument &ia)
+            {
+            }
+        }
+        break;
+    case 1: // escalona_tudo
+        this->escalona_tudo();
+        break;
+    case 2: // escalona
+        this->escalona(com.get_quantidade());
+        break;
+    case 3: // escalona_host
+        this->escalona_host(com.get_host(), com.get_quantidade());
+        break;
+    case 4: // ver_host
+        this->ver_host(com.get_host());
+        break;
+    case 5: // lista_hosts
+        this->lista_hosts();
+        break;
+    case 6: // limpa_host
+        this->limpa_host(com.get_host());
+        break;
+    case 7: // limpa_tudo
+        this->limpa_tudo();
+        break;
+    default:
+        std::cerr << "Comando invalido: " << com.get_id() << std::endl;
+        abort();
+    }
+}
+
+void Escalonador::destruir()
+// Descricao: fecha o arquivo de saída e limpa a fila de hosts.
+// Entrada: nada.
+// Saida: nenhuma.
+{
+    this->arq_saida.close();
+    this->limpa_tudo();
+}
+
+Escalonador::~Escalonador()
+// Descricao: destrutor padrão do tipo Escalonador.
+// Entrada: nada.
+// Saida: nenhuma.
+{
+    this->destruir();
+}
+
+void Escalonador::add_url(std::string url)
+// Descricao: executa o comando de adicionar URL.
+// Entrada: string correspondente à URL a ser adicionada.
+// Saida: nenhuma.
+{
+    // Inicializa os tipos adequados e procura o host.
+    URL url_verificado(url);
+    Host host_url = Host(url);
+    Host_Node *hostPointer = this->fila.get_host(host_url.base_string());
+
+    // Adiciona o host se ele não está presente, e adiciona o URL.
+    if (hostPointer == nullptr)
+    {
+        this->fila.add_host(host_url);
+        hostPointer = this->fila.get_host(host_url.base_string());
+    }
+    hostPointer->host.add_url(url_verificado);
+}
+
+int Escalonador::escalona_tudo()
+// Descricao: escalona todas as URLs presentes (imŕime em ordem no arquivod de saída e
+// deleta do Escalonador).
+// Entrada: nada.
+// Saida: quantidade de URLs escalonadas.
+{
+    // Escalonamos até 2147483647 URLs, que provavelmente é mais que o suficiente, já que
+    // cada URL tem pelo menos 7 bytes ("http://"), passar esse limite exigiria mais que 14Gb.
+    return this->escalona(INT32_MAX);
+}
+
+int Escalonador::escalona(int quantidade)
+// Descricao: escalona no máximo <quantidade> URLs (imŕime em ordem no arquivod de saída e
+// deleta do Escalonador).
+// Entrada: quantidade máxima a ser escalonada.
+// Saida: quantidade de URLs efetivamente escalonadas.
+{
+    erroAssert(quantidade >= 0, "Quantidade de urls a escalonar invalida.");
+    Host_Node *hn = this->fila.get_front_host();
+    int amtEsc = 0;
+
+    // Escalona cada um dos hosts em ordem, até a quantidade atingir o quanto queremos,
+    // ou os URLs presentes acabarem.
+    while (hn != nullptr && amtEsc < quantidade)
+    {
+        amtEsc += this->escalona_host_interno(hn, 1);
+        Host primeiro_anterior = hn->host;
+        this->fila.remove_front_host();
+        this->fila.add_host(primeiro_anterior);
+        hn = this->fila.get_front_host();
+    }
+    return amtEsc;
+}
+
+int Escalonador::escalona_host(std::string host_string, int quantidade)
+// Descricao: escalona no máximo <quantidade> URLs do host (imŕime em ordem no arquivod
+// de saída e deleta do Escalonador).
+// Entrada: quantidade máxima a ser escalonada e string do host do qual devemos escalonar.
+// Saida: quantidade de URLs efetivamente escalonadas.
+{
+    erroAssert(quantidade >= 0, "Quantidade de URLs a escalonar invalida.");
+    Host_Node *hn = this->fila.get_host(host_string);
+    // Note que simplesmente ignoramos hosts inválidos ao invés de gerar erro.
+    if (hn == nullptr)
+        return 0;
+    else
+        return escalona_host_interno(hn, quantidade);
+}
+
+int Escalonador::escalona_host_interno(Host_Node *hn, int quantidade)
+// Descricao: escalona no máximo <quantidade> URLs do host do nó (imŕime em ordem no
+// arquivo de saída e deleta do Escalonador).
+// Entrada: quantidade máxima a ser escalonada e nó do host cujas URLs devemos escalonar.
+// Saida: quantidade de URLs efetivamente escalonadas.
+{
+    erroAssert(hn != nullptr, "No de host invalido.");
+    int amtEsc = 0;
+
+    while (!hn->host.vazio() && amtEsc < quantidade)
+    {
+        this->arq_saida << hn->host.get_first_url()->url << std::endl;
+        hn->host.remove_first_url();
+        amtEsc++;
+    }
+    return amtEsc;
+}
+
+void Escalonador::ver_host(std::string host_string)
+// Descricao: imprime todas URLs do host em ordem, mas não deleta.
+// Entrada: string do host em questão.
+// Saida: nenhuma.
+{
+    URL_Node *un = this->fila.get_host(host_string)->host.get_first_url();
+    while (un != nullptr)
+    {
+        this->arq_saida << un->url.as_string() << std::endl;
+        un = un->proximo;
+    }
+}
+
+void Escalonador::lista_hosts()
+// Descricao: imprime no arquivo de saída todos hosts em ordem.
+// Entrada: nada.
+// Saida: nenhuma.
+{
+    Host_Node *hn = this->fila.get_front_host();
+    while (hn != nullptr)
+    {
+        this->arq_saida << hn->host.base_string() << std::endl;
+        hn = hn->proximo;
+    }
+}
+
+void Escalonador::limpa_host(std::string host_string)
+// Descricao: deleta todas URLs do host.
+// Entrada: nome do host em questão.
+// Saida: nenhuma.
+{
+    Host_Node *no = this->fila.get_host(host_string);
+    erroAssert(no != nullptr, "Host a ser limpo inexistente.");
+    no->host.limpar();
+}
+
+void Escalonador::limpa_tudo()
+// Descricao: deleta todas URLs e hosts do Escalonador.
+// Entrada: nada.
+// Saida: nenhuma.
+{
+    this->fila.clear();
+}
---
+++ artur_gaspar_2020006388/TP/src/fila_hosts_desafio2.cpp
@@ -0,0 +1,150 @@
+//---------------------------------------------------------------------
+// Arquivo      : fila_hosts.cpp
+// Conteudo     : implementacao do tipo Fila_Hosts
+// Autor        : Artur Gaspar da Silva (artur.gaspar@dcc.ufmg.br)
+//---------------------------------------------------------------------
+
+#include "fila_hosts.h"
+
+Host_Node::Host_Node(Host host) : host(host)
+// Descricao: constrói um nó de hosts.
+// Entrada: host a ser colocado no nó.
+// Saida: objeto instanciado.
+{
+    this->proximo = nullptr;
+}
+
+Host_Node::~Host_Node()
+// Descricao: destrutor padrão de nó de hosts.
+// Entrada: nada.
+// Saida: nenhuma.
+{
+    this->host.limpar();
+}
+
+Fila_Hosts::Fila_Hosts()
+// Descricao: constrói uma fila de hosts.
+// Entrada: nada.
+// Saida: objeto instanciado.
+{
+    this->no_frontal = nullptr;
+    tamanho = 0;
+}
+
+void Fila_Hosts::add_host(Host host)
+// Descricao: adiciona um host à fila, caso já não esteja presente.
+// Entrada: host a ser colocado na fila.
+// Saida: nenhuma.
+{
+    // Note que ignoramos a re-adição de hosts já presentes, ao invés de gerar erro.
+    if (this->get_host(host.base_string()) != nullptr)
+    {
+        return;
+    }
+
+    int tamanho = host.get_tamanho();
+
+    Host_Node *hn = this->no_frontal;
+    if (hn == nullptr)
+    {
+        this->no_frontal = new Host_Node(host);
+    }
+    else
+    {
+        while (hn->proximo != nullptr && hn->proximo->host.get_tamanho() <= tamanho)
+        {
+            hn = hn->proximo;
+        }
+        hn->proximo = new Host_Node(host);
+    }
+    this->tamanho++;
+}
+
+Host_Node *Fila_Hosts::get_front_host()
+// Descricao: obtém o primeiro host.
+// Entrada: nada.
+// Saida: primeiro host da fila.
+{
+    return this->no_frontal;
+}
+
+void Fila_Hosts::remove_front_host()
+// Descricao: remove o primeiro host da fila.
+// Entrada: nada.
+// Saida: nenhuma.
+{
+    Host_Node *primeiro = this->no_frontal;
+    this->no_frontal = this->no_frontal->proximo;
+
+    primeiro->host.limpar();
+    delete primeiro;
+    
+    this->tamanho--;
+}
+
+Host_Node *Fila_Hosts::get_host(std::string host_string)
+// Descricao: obtém o nó correspondente ao host desejado, via busca linear.
+// Entrada: string do host desejado.
+// Saida: nó do host em questão.
+{
+    Host_Node *hn = this->no_frontal;
+    while (hn != nullptr && hn->host.base_string() != host_string)
+    {
+        hn = hn->proximo;
+    }
+    return hn;
+}
+
+void Fila_Hosts::remove_host(std::string host_string)
+// Descricao: remove um host da fila, se presente.
+// Entrada: host a ser retirado da fila.
+// Saida: nenhuma.
+{
+    Host_Node *hn = this->no_frontal;
+    Host_Node *hn_anterior = nullptr;
+    while (hn != nullptr && hn->host.base_string() != host_string)
+    {
+        hn_anterior = hn;
+        hn = hn->proximo;
+    }
+    if (hn != nullptr)
+    {
+        if (hn_anterior == nullptr)
+            remove_front_host();
+        else
+        {
+            hn_anterior->proximo = hn->proximo;
+            hn->host.limpar();
+            delete hn;
+        }
+
+        this->tamanho--;
+    }
+}
+
+bool Fila_Hosts::vazia()
+// Descricao: informa se a fila está vazia
+// Entrada: nada.
+// Saida: true se a fila está vazia, falso caso contrário.
+{
+    return this->tamanho == 0;
+}
+
+void Fila_Hosts::clear()
+// Descricao: remove todos os hosts da fila.
+// Entrada: nada.
+// Saida: nenhuma.
+{
+    while (!vazia())
+    {
+        remove_front_host();
+    }
+}
+
+Fila_Hosts::~Fila_Hosts()
+// Descricao: destrutor padrão da fila de hosts.
+// Entrada: nada.
+// Saida: nenhuma.
+{
+    this->clear();
+}
